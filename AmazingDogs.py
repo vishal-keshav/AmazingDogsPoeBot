f897005f868112e28aafde7aee927c1be500754b
Vishal Keshav <vishal.keshav.1993@gmail.com>
2023-06-19 13:26:20 -0700

    Create AmazingDogs.py
    Added AmazingDogs template file

---
+++ AmazingDogs.py
@@ -0,0 +1,22 @@
+"""
+
+Amazing Dogs Bot shows you the amazing world of Dogs.
+
+"""
+from __future__ import annotations
+
+from typing import AsyncIterable
+
+from fastapi_poe import PoeBot, run
+from fastapi_poe.types import QueryRequest
+from sse_starlette.sse import ServerSentEvent
+
+
+class EchoBot(PoeBot):
+    async def get_response(self, query: QueryRequest) -> AsyncIterable[ServerSentEvent]:
+        last_message = query.query[-1].content
+        yield self.text_event(last_message)
+
+
+if __name__ == "__main__":
+    run(EchoBot())
